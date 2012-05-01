b511e76d037941e73f750b30b90c30639b38c85e
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-05-01 19:25:45 +0100

    deleting the repo

--- drupal/sites/all/themes/bestmobile/block--sidebar_banner_2.tpl.php
+++
@@ -1,7 +0,0 @@
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-  <?php print render($title_suffix); ?>
-  <div class="banner small image third">
-    <?php print $content; ?>
-  </div>
-</div>
--- drupal/sites/all/themes/bestmobile/views-view-unformatted--portfolio-categories--block.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$n1 = '';
-foreach ($rows as $id => $row) {
-  $n1 .= $row;
-}
-print ($n1 ? '<section class="filter"><a href="'.url('portfolio').'" >'.t('All').'</a>'.$n1.'</section>' : '');
-?>
